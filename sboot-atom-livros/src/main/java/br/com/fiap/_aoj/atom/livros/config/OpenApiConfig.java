bfefc99a36305ae4b87f928ceec243bca6836a6e
Rafael Fonseca <rafael.alvezf@gmail.com>
2025-01-15 10:20:09 -0300

    srv-livros | Criado estrutura do componente

---
+++ sboot-atom-livros/src/main/java/br/com/fiap/_aoj/atom/livros/config/OpenApiConfig.java
@@ -0,0 +1,26 @@
+package br.com.fiap._aoj.atom.livros.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("")
+                        .version("1.0.0")
+                        .description("API para gerenciar o catálogo de livros")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Documentação Completa")
+                        .url("https://github.com/RafaelAlvez/BooksOnTheWeb"));
+    }
+
+}
---
+++ sboot-atom-livros/src/test/java/br/com/fiap/_aoj/atom/livros/ApplicationTests.java
@@ -0,0 +1,8 @@
+package br.com.fiap._aoj.atom.livros;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+}
